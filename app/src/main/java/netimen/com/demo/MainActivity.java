21aa4824e50461b156f40d999c6da0ed3171ae33
Dmitry Gordeev <netimen@gmail.com>
2015-03-24 11:07:14 +0300

    example: fragment

--- app/src/main/java/netimen/com/demo/MainActivity.java
+++ app/src/main/java/netimen/com/demo/MainActivity.java
@@ -8,28 +8,10 @@
 package netimen.com.demo;
 
 import android.app.Activity;
-import android.widget.TextView;
-
-import com.netimen.annotations.BeanInitScope;
-import com.netimen.annotations.Event;
 
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
-
-import netimen.com.demo.api.Api;
-import netimen.com.demo.api.events.Calc;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
-    @ViewById
-    TextView result;
-
-    @BeanInitScope
-    Api api;
-
-    @Event
-    void calc(Calc calc) {
-        result.setText(" " + (calc.number * calc.number));
-    }
 }
---
+++ app/src/main/java/netimen/com/demo/SquareFragment.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2015 Bookmate.
+ * All Rights Reserved.
+ *
+ * Author: Dmitry Gordeev <netimen@dreamindustries.co>
+ * Date:   24.03.15
+ */
+package netimen.com.demo;
+
+import android.app.Fragment;
+import android.widget.TextView;
+
+import com.netimen.annotations.BeanInitScope;
+import com.netimen.annotations.Event;
+
+import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+import netimen.com.demo.api.Api;
+import netimen.com.demo.api.events.Calc;
+
+@EFragment(R.layout.fragment_square)
+public class SquareFragment extends Fragment {
+    @ViewById
+    TextView result;
+
+    @BeanInitScope
+    Api api;
+
+    @Event
+    void calc(Calc calc) {
+        result.setText(" " + (calc.number * calc.number));
+    }
+}
