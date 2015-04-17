1f2b977627dd7a2305fe60d6e2c7b5083da2d3b0
Dmitry Gordeev <netimen@gmail.com>
2015-04-17 14:10:09 +0300

    new demo: maps

---
+++ androidmodulesdemo/src/main/java/com/netimen/androidmodules/demo/DemoActivity.java
@@ -0,0 +1,9 @@
+package com.netimen.androidmodules.demo;
+
+import android.support.v4.app.FragmentActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_demo)
+public class DemoActivity extends FragmentActivity {
+}
