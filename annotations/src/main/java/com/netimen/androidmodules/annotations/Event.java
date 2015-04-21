6d75dbd70643542d3ade57adfb10f78201ee0d95
Dmitry Gordeev <netimen@gmail.com>
2015-04-21 11:50:52 +0300

    Request & event javadocs

--- annotations/src/main/java/com/netimen/androidmodules/annotations/Event.java
+++ annotations/src/main/java/com/netimen/androidmodules/annotations/Event.java
@@ -13,6 +13,36 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to subscribe to an event. By default, it uses the {@link com.netimen.androidmodules.helpers.Bus} instance stored in the class annotated with {@link EModule}, so every submodule uses the same bus.
+ *
+ * <pre>
+ * &#064;EBean
+ * public class Submodule {
+ *
+ * 	&#064;Event
+ * 	void someEvent() { // subscribes to SomeEvent event, event name is inferred from method name
+ * 	}
+ *
+ * 	&#064;Event
+ * 	void onOtherEvent() { // subscribes to OtherEvent event, event name is inferred from method name, 'on' is omitted
+ * 	}
+ *
+ * 	&#064;Event
+ * 	void thirdEventHappened(ThirdEvent event) { // subscribes to ThirdEvent event, event name is inferred from parameter; method name doesn't matter
+ * 	}
+ *
+ *
+ * 	&#064;Event(FourthEvent.class)
+ * 	void fourthEventHappened() { // subscribes to FourthEvent event
+ * 	}
+ *
+ * 	&#064;Event(moduleClass = {FragmentOne.class, FragmentTwo.class})
+ * 	void fithEvent() { // subscribes to FithEvent event, coming from submodules of modules FragmentOne or FragmentTwo. May be useful in the activity if it has several modularized fragment.
+ * 	}
+ * }
+ * </pre>
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
 @SuppressWarnings("UnusedDeclaration")
--- annotations/src/main/java/com/netimen/androidmodules/annotations/Request.java
+++ annotations/src/main/java/com/netimen/androidmodules/annotations/Request.java
@@ -12,6 +12,32 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to subscribe to a request. By default, it uses the {@link com.netimen.androidmodules.helpers.Bus} instance stored in the class annotated with {@link EModule}, so every submodule uses the same bus.
+ * The difference between request and event is that event is used only to notify about something, and request is used to get some data from other submodule.
+ *
+ * <pre>
+ * &#064;EBean
+ * public class Submodule {
+ *
+ * 	&#064;Request
+ * 	Integer getId() { // subscribes to GetId request, request name is inferred from method name
+ * 	    return ...;
+ * 	}
+ *
+ * 	&#064;Request
+ * 	String handleGetAuthor(GetAuthor request) { // subscribes to ThirdEvent event, event name is inferred from parameter; method name doesn't matter
+ * 	    return ...;
+ * 	}
+ *
+ *
+ * 	&#064;Request(IsActive.class)
+ * 	boolean isActive() {
+ * 	    return ...;
+ * 	}
+ * }
+ * </pre>
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
 @SuppressWarnings("UnusedDeclaration")
